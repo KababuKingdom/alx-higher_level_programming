20f046b61e100577eae2d1823f6d727c7fa145b5
KababuKingdom <kababukingdom@gmail.com>
2022-12-09 01:10:21 +0000

    10. Best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
-def best_score(my_dictionary):
-    if my_dictionary:
-        return (max(my_dictionary))
+def best_score(a_dictionary):
+    """Returns a key with the biggest integer value."""
+    if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
+        return None
+
+    ret = list(a_dictionary.keys())[0]
+    big = a_dictionary[ret]
+    for x, y in a_dictionary.items():
+        if y > big:
+            big = y
+            ret = x
+    return (ret)
