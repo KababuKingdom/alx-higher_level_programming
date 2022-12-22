c41d2c762916de93aff9d6dd6e18f60d700b0e22
KababuKingdom <kababukingdom@gmail.com>
2022-12-22 23:44:38 +0000

    a program that prints the number of and the list of its arguments.

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,21 +1,14 @@
 #!/usr/bin/python3
-import sys
-
-if __name__ == '__main__':
-    """Prints the argument list passed to the program
-    The program takes all the arguments starting from the second
-    and prints the number of arguments and their value
-    """
-    av = sys.argv
-    l_av = len(av) - 1
-
-    if l_av > 1:
-        print(l_av, 'arguments:')
-        for i in range(1, l_av + 1):
-            print('{:d}: {}'.format(i, av[i]))
-    elif l_av == 1:
-        print(l_av, 'argument:')
-        for i in range(1, l_av + 1):
-            print('{:d}: {}'.format(i, av[i]))
-    elif l_av == 0:
-        print(l_av, 'arguments.')
+if __name__ == "__main__":
+    from sys import argv
+    n = len(argv)
+    n = n - 1
+    if n == 0:
+        print("{:d} arguments.".format(n))
+    elif n == 1:
+        print("{:d} argument:".format(n))
+        print("{:d}: {}".format(n, argv[n]))
+    else:
+        print("{:d} arguments:".format(n))
+        for i in range(1, n + 1):
+            print("{:d}: {}".format(i, argv[i]))
