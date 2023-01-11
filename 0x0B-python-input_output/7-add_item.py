d35279e77c476a03293524d37bb8da38753efec8
KababuKingdom <kababukingdom@gmail.com>
2023-01-11 23:27:41 +0000

    Mandatory Task

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,17 +1,12 @@
 #!/usr/bin/python3
-"""Module that loads, adds and saves arguments to a Python list"""
-from sys import argv
+
+import json
 
 
-load_file = __import__('6-load_from_json_file').load_from_json_file
-save_file = __import__('5-save_to_json_file').save_to_json_file
+def save_to_json_file(my_obj, filename):
+    """function that writes an Object to a text file,
+    using JSON representation"""
 
-try:
-    json_list = load_file('add_item.json')
-except (ValueError, FileNotFoundError):
-    json_list = []
-
-for item in argv[1:]:
-    json_list.append(item)
-
-save_file(json_list, 'add_item.json')
+    with open(filename, 'w') as f:
+        filename = f.write(json.dumps(my_obj))
+    return filename
