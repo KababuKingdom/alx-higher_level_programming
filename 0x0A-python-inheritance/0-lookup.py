77d2f6d9b74c438fde6d33fee7fb44791eebc7f8
KababuKingdom <kababukingdom@gmail.com>
2023-01-11 22:45:48 +0000

    Task 0

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-"""
-    0-lookup: lookup()
-"""
+"""Returns a function lookup()"""
+
 
 def lookup(obj):
+    """Function that returns the list of available attributes and methods,
+    f an object
+    Args:
+        obj (class): object
+    Returns:
+        list: list of available attributes and methods of an object
     """
-        Returns the list of available attributes and methods.
-        Args:
-            obj (object): object.
-    """
-    return (dir(obj)
+    return dir(obj)
