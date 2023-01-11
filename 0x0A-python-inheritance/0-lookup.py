7604fb4ca9d6c6c8565405d94db43ef880cc9832
KababuKingdom <kababukingdom@gmail.com>
2023-01-11 22:42:26 +0000

    Task 0

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+    0-lookup: lookup()
+"""
+
+def lookup(obj):
+    """
+        Returns the list of available attributes and methods.
+        Args:
+            obj (object): object.
+    """
+    return (dir(obj))
