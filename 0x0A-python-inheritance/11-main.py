174cb462e842dba0f1687988ba22c5bc063093dc
KababuKingdom <kababukingdom@gmail.com>
2023-01-11 23:17:23 +0000

    Mandatory Tasks

--- 0x0A-python-inheritance/11-main.py
+++ 0x0A-python-inheritance/11-main.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-"""Defines a class Square based on 9-rectangle.py.
-Attributes:
-    width (int): width of the rectangle.
-    height (int): height of the rectangle.
+"""
+    11-square: class Square from Rectangle
 """
 
 
@@ -10,30 +8,28 @@ Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """Defines a class Square.
-    Args:
-        Rectangle (Rectangle): rectangle
     """
-
+        Square  inherits from Rectangle
+        Attributes:
+            size (int): side of square
+        Methods:
+            __init__ - initialises the square
+    """
     def __init__(self, size):
-        """Creates new instances of class Square.
-        Args:
-            size (int): size of 1 side of square.
+        """
+            initialises Square
         """
         self.integer_validator("size", size)
-        self.__size = size
-        super().__init__(size, size)
 
-    def __str__(self):
-        """Returns a string representation of the square.
-        Returns:
-            str: square.
-        """
-        return ("[Square] {}/{}".format(self.__size, self.__size))
+        self.__size = size
 
     def area(self):
-        """Calculates the area of a square.
-        Returns:
-            int: the area of the square.
         """
-        return self.__size ** 2
+            Returns the area of square
+        """
+        area = self.__size * self.__size
+        return area
+
+    def __str__(self):
+        return ("[{}] {}/{}".format(type(self).__name__,
+                                    self.__size, self.__size))
