3e02b4896a3adeeffb14a4319522daca73b15ed6
KababuKingdom <kababukingdom@gmail.com>
2023-01-12 00:26:30 +0000

    Advanced Tasks

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -3,75 +3,39 @@
 
 import sys
 
-def print_solution(board):
-    """
-    Prints the board configuration of a given solution
-    """
-    for row in board:
-        print(" ".join(row))
+def solve_nqueens(n):
+    def is_safe(board, row, col):
+        for r in range(row):
+            if board[r] == col or abs(board[r] - col) == abs(r - row):
+                return False
+        return True
 
-def is_safe(board, row, col):
-    """
-    Returns True if it is safe to place a queen at (row, col) on the board,
-    False otherwise
-    """
-    # Check if there is a queen in the same column
-    for i in range(row):
-        if board[i][col] == "Q":
-            return False
+    def backtrack(board, row):
+        if row == n:
+            solutions.append(board[:])
+            return
+        for col in range(n):
+            if is_safe(board, row, col):
+                board[row] = col
+                backtrack(board, row + 1)
 
-    # Check if there is a queen in the same \ diagonal
-    i, j = row, col
-    while i >= 0 and j >= 0:
-        if board[i][j] == "Q":
-            return False
-        i -= 1
-        j -= 1
-
-    # Check if there is a queen in the same / diagonal
-    i, j = row, col
-    while i >= 0 and j < len(board):
-        if board[i][j] == "Q":
-            return False
-        i -= 1
-        j += 1
-
-    return True
-
-def solve_nqueens(board, row):
-    """
-    Solves the N queens problem by trying to place a queen in every
-    possible position in the current row and recursing to the next row
-    """
-    if row == len(board):
-        print_solution(board)
-        print()
-        return
-
-    for col in range(len(board)):
-        if is_safe(board, row, col):
-            board[row][col] = "Q"
-            solve_nqueens(board, row + 1)
-            board[row][col] = "."
-
-def main():
-    if len(sys.argv) != 2:
-        print("Usage: nqueens N")
-        sys.exit(1)
-
-    n = sys.argv[1]
-    try:
-        n = int(n)
-    except ValueError:
-        print("N must be a number")
-        sys.exit(1)
-
-    if n < 4:
-        print("N must be at least 4")
-        sys.exit(1)
-
-    board = [["." for _ in range(n)] for _ in range(n)]
-    solve_nqueens(board, 0)
+    solutions = []
+    board = [-1] * n
+    backtrack(board, 0)
+    return solutions
 
 if __name__ == "__main__":
-    main()
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N", file=sys.stderr)
+        sys.exit(1)
+    try:
+        n = int(sys.argv[1])
+    except ValueError:
+        print("N must be a number", file=sys.stderr)
+        sys.exit(1)
+    if n < 4:
+        print("N must be at least 4", file=sys.stderr)
+        sys.exit(1)
+    solutions = solve_nqueens(n)
+    for solution in solutions:
+        print(solution)
